3c6bfd4b9113273407aa62a55df4091de27722c7
johslsks-svg <johslsks@gmail.com>
2025-09-15 20:11:41 +0800

    Update get_promos.js

--- api/get_promos.js
+++ api/get_promos.js
@@ -5,6 +5,5 @@ export default function handler(req, res) {
   const filePath = path.join(process.cwd(), 'promos.json');
   const data = fs.readFileSync(filePath, 'utf8');
   const promos = JSON.parse(data);
-
   res.status(200).json(promos);
 }
